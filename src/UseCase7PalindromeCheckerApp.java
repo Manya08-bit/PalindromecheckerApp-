bc1a8100fe6c36949cfea82e36f5eb6faa413e8e
Manya08-bit <mk6801@srmist.edu.in>
2026-02-24 15:57:30 +0530

    Added UC7 Deque-Based Optimized Palindrome Checker

---
+++ src/UseCase7PalindromeCheckerApp.java
@@ -0,0 +1,40 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class UseCase7PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Original string
+        String original = "racecar";
+
+        // Create Deque
+        Deque<Character> deque = new LinkedList<>();
+
+        // Insert characters into deque
+        for (int i = 0; i < original.length(); i++) {
+            deque.addLast(original.charAt(i));
+        }
+
+        // Assume it is palindrome
+        boolean isPalindrome = true;
+
+        // Compare front and rear
+        while (deque.size() > 1) {
+            char front = deque.removeFirst();
+            char rear = deque.removeLast();
+
+            if (front != rear) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Print result
+        if (isPalindrome) {
+            System.out.println(original + " is a Palindrome.");
+        } else {
+            System.out.println(original + " is NOT a Palindrome.");
+        }
+    }
+}
